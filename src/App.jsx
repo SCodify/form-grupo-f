99af105f5ce93fea3e40dcbacfc6024c5b815f35
Christian Ledesma <softcodify@gmail.com>
2023-11-15 16:19:41 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='container-fluid bg-primary text-light text-center p-5'>Test</h1>
+    </>
+  )
+}
+
+export default App
